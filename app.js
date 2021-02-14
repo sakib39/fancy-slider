c753902b032a6fe7cf2b4c9a34372d9d451e606f
sakib39 <snajmus16@gmail.com>
2021-02-14 12:10:46 +0600

    complete

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const galleryHeader = document.querySelector('.gallery-header');
 const searchBtn = document.getElementById('search-btn');
 const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
+const search = document.getElementById('search');
+const searchHeader = document.getElementById('search-header');
 // selected image 
 let sliders = [];
 
@@ -15,36 +17,39 @@ const KEY = '15674931-a9d714b6e9d654524df198e00&q';
 
 // show images 
 const showImages = (images) => {
+  // imagesArea.innerHTML = '';
   imagesArea.style.display = 'block';
   gallery.innerHTML = '';
+  searchHeader.innerText = `showing ${search.value} images`;
   // show gallery title
   galleryHeader.style.display = 'flex';
   images.forEach(image => {
     let div = document.createElement('div');
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
-    gallery.appendChild(div)
+    gallery.appendChild(div);
   })
-
+  toggleSpinner();
 }
 
 const getImages = (query) => {
+  toggleSpinner();
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hitS))
+    .then(data => showImages(data.hits))
     .catch(err => console.log(err))
 }
 
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
+  element.classList.toggle('added');
  
   let item = sliders.indexOf(img);
-  if (item === -1) {
+  if (item === -1 && element.classList.contains('added')) {
     sliders.push(img);
-  } else {
-    alert('Hey, Already added !')
+  }else{
+    sliders.shift(img);
   }
 }
 var timer
@@ -68,19 +73,28 @@ const createSlider = () => {
   // hide image aria
   imagesArea.style.display = 'none';
   const duration = document.getElementById('duration').value || 1000;
-  sliders.forEach(slide => {
-    let item = document.createElement('div')
-    item.className = "slider-item";
-    item.innerHTML = `<img class="w-100"
-    src="${slide}"
-    alt="">`;
-    sliderContainer.appendChild(item)
-  })
-  changeSlide(0)
-  timer = setInterval(function () {
-    slideIndex++;
-    changeSlide(slideIndex);
-  }, duration);
+  if(duration > 0){
+    sliders.forEach(slide => {
+      let item = document.createElement('div')
+      item.className = "slider-item";
+      item.innerHTML = `<img class="w-100"
+      src="${slide}"
+      alt="">`;
+      sliderContainer.appendChild(item)
+    })
+    changeSlide(0)
+    timer = setInterval(function () {
+      slideIndex++;
+      changeSlide(slideIndex);
+    }, duration);
+  }else{
+    let lessDuration = document.createElement('div');
+    lessDuration.className = "less-duration";
+    lessDuration.innerHTML = `
+      <h3 class="text-center">Duration should be positive</h3>
+    `;
+    sliderContainer.appendChild(lessDuration);
+  }
 }
 
 // change slider index 
@@ -103,20 +117,36 @@ const changeSlide = (index) => {
   }
 
   items.forEach(item => {
-    item.style.display = "none"
+    item.style.display = "none";
   })
 
-  items[index].style.display = "block"
+  items[index].style.display = "block";
+}
+
+const toggleSpinner = () => {
+  const spinner = document.getElementById('spinner');
+  spinner.classList.toggle('d-none');
 }
 
 searchBtn.addEventListener('click', function () {
   document.querySelector('.main').style.display = 'none';
   clearInterval(timer);
-  const search = document.getElementById('search');
   getImages(search.value)
   sliders.length = 0;
 })
 
 sliderBtn.addEventListener('click', function () {
   createSlider()
+})
+
+search.addEventListener("keypress",function(event){
+  if(event.key === 'Enter'){
+    searchBtn.click();
+  }
+})
+
+duration.addEventListener("keypress",function(event){
+  if(event.key === 'Enter'){
+    sliderBtn.click();
+  }
 })
